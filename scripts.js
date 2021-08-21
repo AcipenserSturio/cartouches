c8c4e85d64d9ecaeab4532f96a41bc0f3c7edd34
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2021-08-21 11:34:46 +0500

    It's alive!

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
+// Auxilliary functions
+
 String.prototype.toTitleCase = function () {
     return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
 Array.prototype.differenceOfArrays = function(arr2) { return this.filter(x => !arr2.includes(x)); }
 
+function chooseRandom(arr) {
+	return arr[Math.floor(Math.random() * arr.length)]
+}
+
+// Data
 const words = ["a", "akesi", "ala", "alasa", "ale", "anpa", "ante", "anu", "awen", "e", "en", "esun", "ijo", "ike", "ilo", "insa", "jaki", "jan", "jelo", "jo", "kala", "kalama", "kama", "kasi", "ken", "kepeken", "kili", "kiwen", "ko", "kon", "kule", "kulupu", "kute", "la", "lape", "laso", "lawa", "len", "lete", "li", "lili", "linja", "lipu", "loje", "lon", "luka", "lukin", "lupa", "ma", "mama", "mani", "meli", "mi", "mije", "moku", "moli", "monsi", "mu", "mun", "musi", "mute", "nanpa", "nasa", "nasin", "nena", "ni", "nimi", "noka", "o", "olin", "ona", "open", "pakala", "pali", "palisa", "pan", "pana", "pi", "pilin", "pimeja", "pini", "pipi", "poka", "poki", "pona", "pu", "sama", "seli", "selo", "seme", "sewi", "sijelo", "sike", "sin", "sina", "sinpin", "sitelen", "sona", "soweli", "suli", "suno", "supa", "suwi", "tan", "taso", "tawa", "telo", "tenpo", "toki", "tomo", "tu", "unpa", "uta", "utala", "walo", "wan", "waso", "wawa", "weka", "wile"]
 const words_banned_as_honorifics = ["a", "anu", "e", "en", "kepeken", "la", "li", "lon", "mi", "ni", "o", "ona", "pi", "sama", "seme", "sina", "tan", "taso", "tawa", "unpa"]
 const syllable_counts = [1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4]
@@ -11,6 +18,7 @@ const word_internal = ["ma", "me", "mi", "mo", "mu", "na", "ne", "ni", "no", "nu
 const word_internal_preceded_by_n = ["pa", "pe", "pi", "po", "pu", "ta", "te", "to", "tu", "ka", "ke", "ki", "ko", "ku", "sa", "se", "si", "so", "su", "wa", "we", "wi", "la", "le", "li", "lo", "lu", "ja", "je", "jo", "ju"]
 const coda_n = ["", "", "", "", "", "n"]
 
+// Dynamically generated data
 const honorifics = words.differenceOfArrays(words_banned_as_honorifics)
 cartouche_dictionary = {"a": [], "e": [], "i": [], "o": [], "u": [], "m": [], "n": [], "p": [], "t": [], "k": [], "s": [], "w": [], "l": [], "j": []}
 for (i = 0; i < words.length; i++) {
@@ -20,23 +28,28 @@ for (i = 0; i < words.length; i++) {
 
 var current_word
 
-function onNewWord() {
+function userEntersSomething() {
 	word = document.getElementById("word").value
+	//console.log(word)
+	if (word == current_word) {
+		onNewWord()
+	}
+}
+
+function onNewWord() {
 	document.getElementById("word").value = ""
 	//alert(word)
 	setNewWord()
 }
 
 function setNewWord() {
+	honorific = chooseRandom(honorifics)
 	generated_word = generateValidWord()//.toTitleCase()
 	generated_cartouche = generateCartoucheFromWord(generated_word)
-	current_word = chooseRandom(honorifics) + " " + generated_cartouche
+	displayed_word = honorific + " " + generated_cartouche
+	current_word = honorific + " " + generated_word.toTitleCase()
 	//alert(current_word)
-	document.getElementById("word_display").innerHTML = current_word
-}
-
-function chooseRandom(arr) {
-	return arr[Math.floor(Math.random() * arr.length)]
+	document.getElementById("word_display").innerHTML = displayed_word
 }
 
 function generateValidWord() {
